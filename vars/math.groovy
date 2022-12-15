5bc3107cd8b2e9e5382ec2916a75b275c4af81e8
Prashant43129 <120032711+Prashant43129@users.noreply.github.com>
2022-12-15 16:07:56 +0530

    Update math.groovy

--- vars/math.groovy
+++ vars/math.groovy
@@ -2,3 +2,11 @@ def sum(a,b)
 {
    echo "the sum of ${a} and ${b} is ${a+b}"
 }
+def mul(a,b,c)
+{
+   echo " the product of ${a} and ${b} and ${c} is ${a*b*c}"
+}
+def average(a,b,c)
+{
+   echo "the average of ${a},${b} and ${c} is ${a+b+c/3}
+}
