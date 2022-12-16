204db319624710959b437fa30c386a117f63ab09
Prashant43129 <120032711+Prashant43129@users.noreply.github.com>
2022-12-16 15:31:02 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -10,3 +10,11 @@ def deploy(jobname,ip,appname)
 {
     sh "scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${appname}.war"
 }
+def testing(repo)
+{
+   git "https://github.com/intelliqittrainings/${repo}"
+}
+def delivery(jobname,ip,appname)
+{
+    sh "scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${appname}.war"
+}
